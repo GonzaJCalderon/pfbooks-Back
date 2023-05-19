8e2fd5df5679caccfbe172a2c0e3acac75ef5d6d
Gonzalo Calderon <gonzacalderon13788@gmail.com>
2023-05-19 01:40:13 -0300

    cambio port

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const loadDataFromApi = require("./src/dbload/loadDataBase");
 require('dotenv').config();
-const { PORT } = process.env.PORT || 3001
+
+const { PORT } = process.env.PORT 
 
 conn.sync({ alter: true })
     .then(() => {
-        server.listen(PORT, () => {
+        server.listen(PORT || 3001, () => {
             console.log(`server listening at ${PORT}`);
         });
     })
