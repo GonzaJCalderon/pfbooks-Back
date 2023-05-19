0e6e5d03ced84b12e4b37e13c4000ae60c8f9166
Gonzalo Calderon <gonzacalderon13788@gmail.com>
2023-05-19 01:44:31 -0300

    cambio en index

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const { conn } = require('./src/db.js');
 const loadDataFromApi = require("./src/dbload/loadDataBase");
 require('dotenv').config();
 
-const { PORT } = process.env.PORT 
+// const { PORT } = process.env.PORT 
 
-conn.sync({ alter: true })
-    .then(() => {
-        server.listen(PORT || 3001, () => {
-            console.log(`server listening at ${PORT}`);
-        });
-    })
+conn.sync({ alter: true }).then(() => {
+    server.listen(process.env.PORT || 3001, () => {
+      console.log(`Server listening on port ${process.env.PORT || 3001}`);
+    });
+  })
     .then(() => {
         loadDataFromApi();
     });
