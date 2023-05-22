6ffd4a271adaeb5b5183011a6346fb5a982d80d7
Gonzalo Calderon <gonzacalderon13788@gmail.com>
2023-05-22 00:40:28 -0300

    actualizacion user

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const loadDataFromApi = require("./src/dbload/loadDataBase");
-require('dotenv').config();
 
-// const { PORT } = process.env.PORT 
-
-conn.sync({ alter: true }).then(() => {
-    server.listen(process.env.PORT || 3001, () => {
-      console.log(`Server listening on port ${process.env.PORT || 3001}`);
-    });
-  })
+conn.sync({ alter: true })
+    .then(() => {
+        server.listen(3001, () => {
+            console.log('server listening at 3001');
+        });
+    })
     .then(() => {
         loadDataFromApi();
     });
