a83046732c16c5e74faee61d0014448c14c8d6ba
Gonzalo Calderon <gonzacalderon13788@gmail.com>
2023-05-30 03:11:30 -0300

    cambio en axios

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use(express.json());
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+res.header('Access-Control-Allow-Origin', 'http://localhost:3000'  || 'https://front-serendipia.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
