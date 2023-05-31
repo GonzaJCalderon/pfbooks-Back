37c69fc323d51cd6a4a09138a0e7bfbe32796d8c
Gonzalo Calderon <gonzacalderon13788@gmail.com>
2023-05-30 21:02:12 -0300

    cambios en rutas-modelos-controllers

--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -15,7 +15,7 @@ bookRouter.get("/", async (req, res) => {
     const { title , genre, author } = req.query;
     let books;
     try {
-        if(title || genre || author) {
+        if( title || genre || author ) {
             books = await getBooksByCombinedFilters(title, genre, author)
         } else {
             books = await getAllBooks()
@@ -44,10 +44,10 @@ bookRouter.get("/:bookId",async (req, res) => {
 
 /// RUTA UPDATE BOOK
 bookRouter.put('/update',async (req, res) => {
-    // validateJWT(req, true);
-    const { id, title, author, price, image, genre, rating, stock, description } = req.body;
+    validateJWT(req, true);
+    const { id, title, author, price, image, genre, rating, stock, description, availability } = req.body;
     try {
-        const enablementUser = ( await updateBookById(id, title, author, price, image, genre, rating, stock, description) );
+        const enablementUser = ( await updateBookById(id, title, author, price, image, genre, rating, stock, description, availability) );
         res.status(200).json( enablementUser );
     }
     catch (error){
@@ -57,7 +57,7 @@ bookRouter.put('/update',async (req, res) => {
 
 //RUTA POST NEW BOOK
 bookRouter.post('/addBook',async (req, res) => {
-    // validateJWT(req, true);
+    validateJWT(req, true);
     const { title, author, price, image, genre, rating, stock, description } = req.body;
     try {
         const newBook = ( await addNewBook( title, author, price, image, genre, rating ,stock, description) );
