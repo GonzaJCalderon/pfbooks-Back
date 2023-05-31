a15323b9cb46180472f13eb5809a49182ca977a5
Jorge Jimenez <jorge970102@outlook.com>
2023-05-31 16:35:22 -0500

    model Reviews

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const loadDataFromApi = require("./src/dbload/loadDataBase");
 require('dotenv').config();
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
     server.listen(process.env.PORT || 3001, () => {
       console.log(`Server listening on port ${process.env.PORT || 3001}`);
     });
